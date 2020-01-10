aedc5b2816059cfb7a255ecaed95ef6b86fec104
Emil Manolov <emil.manolov@holidayextras.com>
2020-01-10 13:58:35 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  rules: {
+    'no-template-literals': {
+      create: (context) => ({
+        TemplateLiteral (node) {
+          context.report(node, 'Do not use template literals!!!!!!1')
+        }
+      })
+    },
+    'async-func-name': {
+      create: (context) => ({
+        FunctionDeclaration (node) {
+          if (node.async && !/Async$/.test(node.id.name)) {
+            context.report({
+              node,
+              message: "Async function name '{{ funcname }}' must end with 'Async'!!!!!!! (number of params {{params}})",
+              data: {
+                funcname: node.id.name,
+                params: node.params.length
+              }
+            })
+          }
+        }
+      })
+    }
+  }
+}
