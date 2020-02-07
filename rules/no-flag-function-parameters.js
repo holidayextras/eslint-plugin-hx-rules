34407d79be3215f721eab0d737634ef194274ff4
Emil Manolov <emil.manolov@holidayextras.com>
2020-02-07 16:54:09 +0200

    Add meta

--- rules/no-flag-function-parameters.js
+++ rules/no-flag-function-parameters.js
@@ -1,6 +1,15 @@
 module.exports = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: 'disallow usage of flag function parameters',
+      category: 'Possible Errors',
+      recommended: true,
+      url: 'https://github.com/holidayextras/eslint-plugin-hx-rules/docs/rules/no-flag-function-parameters'
+    },
+    schema: []
+  },
   create: (context) => ({
-    // 'IfStatement > LogicalExpression > Identifier': (node) => {
     'IfStatement Identifier': (node) => {
       const functionDeclaration = context.getAncestors().find(a => a.type === 'FunctionDeclaration')
       if (functionDeclaration && functionDeclaration.params.map(p => p.name).includes(node.name)) {
